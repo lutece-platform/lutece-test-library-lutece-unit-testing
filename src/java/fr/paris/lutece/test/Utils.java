68a733d8446b597d02e4f458014c30514822c078
pierrelevy <pierrelevy@users.noreply.github.com>
2012-08-01 10:04:05 +0000

    JUNIT-3 : Add Utils to load the content of test files located in src/test/resources
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/library/library-lutece-unit-testing@42749 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/java/fr/paris/lutece/test/Utils.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2002-2012, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+/**
+ * Utils for tests
+ */
+public class Utils
+{
+    private static Utils _singleton = new Utils(  );
+
+    /**
+     * Gets the content of a file as a string
+     * @param strFilename The filename (ie: myfile.text for file stored in src/test/resources)
+     * @return The file's content
+     * @throws IOException If an IO error occurs
+     */
+    public static String getFileContent( String strFilename )
+                                 throws IOException
+    {
+        InputStream is = _singleton.getClass(  ).getResourceAsStream( "/" + strFilename );
+        InputStreamReader isr = new InputStreamReader( is );
+        BufferedReader in = new BufferedReader( isr );
+        Writer writer = new StringWriter(  );
+
+        if ( in != null )
+        {
+            char[] buffer = new char[1024];
+
+            try
+            {
+                int n;
+
+                while ( ( n = in.read( buffer ) ) != -1 )
+                {
+                    writer.write( buffer, 0, n );
+                }
+            } finally
+            {
+                isr.close(  );
+            }
+
+            return writer.toString(  );
+        } else
+        {
+            return "";
+        }
+    }
+}
